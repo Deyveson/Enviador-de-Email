2ce195c261420c25f60f62a70855e66e510ede28
deyveson <deyvesonn@outlook.com>
2018-11-12 16:59:19 -0400

    Subindo o Servico de Email

---
+++ src/main/java/br/com/alura/enviadorEmail/enviadorEmail/EmailService.java
@@ -0,0 +1,34 @@
+package br.com.alura.enviadorEmail.enviadorEmail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+// dizendo que essa classe e um servico do Spring 
+public class EmailService {
+	
+	public void enviar (String nome, String emailDestinatario) {
+		
+		 try {
+	            Email email = new SimpleEmail();
+	            email.setHostName("smtp.googlemail.com");
+	            email.setSmtpPort(587);
+	            email.setAuthenticator(new DefaultAuthenticator("deyvesonn4@gmail.com", "38223600159"));
+	            email.setSSLOnConnect(true);
+
+	            email.setFrom("deyvesonn4@gmail.com");
+	            email.setSubject("Você foi convidado pelo ListaVIP");
+	            email.setMsg("Olá " + nome + ". Você acaba de ser convidado pelo ListaVIP.");
+	            email.addTo(emailDestinatario);
+	            email.send();
+
+	        } catch (EmailException e) {
+	            e.printStackTrace();
+	        }
+		
+	}
+	
+}
